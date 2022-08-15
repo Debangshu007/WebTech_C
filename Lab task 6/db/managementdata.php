800c3d3a3427b4b8262611876fa696e8bd163292
Debangshu Pal Sourav <61140103+Debangshu007@users.noreply.github.com>
2022-08-16 00:04:27 +0600

    Add files via upload

---
+++ Lab task 6/db/managementdata.php
@@ -0,0 +1,1 @@
+[[{"name":"Debangshu Pal Sourav","email":"sourov.debangshu@gmail.com","password":"123456789@","gender":"male","nid":"0000000000","location":"Dhaka","dob":"2021-03-02"}],[{"name":"Debangshu","email":"Dsp@gmail.com","password":"123456789@","gender":"male","nid":"1111111111","location":"Dhaka","dob":"2021-03-12"}]]
---
+++ Lab task 6/templates/footer.php
@@ -0,0 +1,10 @@
+<?php
+function footer_section()
+{
+?>
+    <footer>
+        <h2> Designed by <a href="https://github.com/Debangshu007">Safayet Hossain </a></h2>
+    </footer>
+    </body>
+
+    </html><?php } ?>
